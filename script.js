ff181e2e2c2402de891e118b3fc14576c3dc22aa
Shakir Hussain <shakir.hussain6969@gmail.com>
2022-12-20 00:34:08 +0500

    Fixed ESlint issues

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var nav = document.querySelector('.page-flow');
-var ham = document.querySelector('.hamburger');
-var body = document.querySelector('body');
-var li = document.querySelectorAll('.page-flow > li');
-var x = window.matchMedia('(max-width: 768px)');
-var sec = document.querySelectorAll('*');
-myFunction(x);
+const nav = document.querySelector('.page-flow');
+const ham = document.querySelector('.hamburger');
+const body = document.querySelector('body');
+let li = document.querySelectorAll('.page-flow > li');
+let x = window.matchMedia('(max-width: 768px)');
+let sec = document.querySelectorAll('*');
 let id;
+
 function myFunction(x) {
   // If media query matches
   if (x.matches) {
@@ -28,14 +28,17 @@ function myFunction(x) {
     body.classList.remove('b_active');
   }
 }
+
+myFunction(x);
+
 sec.forEach((section) => {
   section.addEventListener('mouseenter', () => {
     id = section.getAttribute('id');
-    if (id == 'about-myself') {
+    if (id === 'about-myself') {
       li[0].classList.remove('active-page');
       li[1].classList.add('active-page');
       li[2].classList.remove('active-page');
-    } else if (id == 'contact') {
+    } else if (id === 'contact') {
       li[0].classList.remove('active-page');
       li[1].classList.remove('active-page');
       li[2].classList.add('active-page');
