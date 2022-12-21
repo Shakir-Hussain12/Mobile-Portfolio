554a3f432160dbb65369a747d1e3bb9f50c06b56
Shakir Hussain <shakir.hussain6969@gmail.com>
2022-12-21 13:44:39 +0500

    Added style and JS for pop-up window

--- script.js
+++ script.js
@@ -6,9 +6,38 @@ const sec = document.querySelectorAll('*');
 const show = document.querySelector('#showDetails')
 const btnExpand = document.querySelectorAll('.normal-button');
 const btnCollapse = document.querySelector('.closeBtn')
-
-let id;
+const checktitle = document.querySelectorAll('.work-item');
+let nav_id;
 let screenWidth = window.innerWidth;
+
+let showData = {
+  title: [
+    'Tonic','Multi-Post Stories','Facebook 360', 'Uber navigation'
+  ],
+  detail: [
+    [
+      'CANOPY','FACEBOOK','FACEBOOK','UBER'
+    ],
+    [
+      'Back End Dev','Full Stack Dev','Full Stack Dev','Lead Developer'
+    ]
+  ],
+  info: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+  tech: [
+    './images/Tag.png','./images/Tag (1).png','./images/Tag(2).png','./desktop-images/Tag.png'
+  ],
+  src:[
+    './desktop-images/Snapshoot\ Portfolio.png','./desktop-images/Snapshoot\ Portfolio-1.png','./desktop-images/Snapshoot\ Portfolio-2.png','./desktop-images/Snapshoot\ Portfolio-3.png'
+  ],
+  buttons: [
+    [
+      'https://shakir-hussain12.github.io/Mobile-Portfolio/','https://github.com/Shakir-Hussain12/Mobile-Portfolio'
+    ],
+    [
+      './desktop-images/Live_btn.png','./desktop-images/Source_Btn.png'
+    ]
+  ]
+}
 
 function remAttributes() {
   nav.classList.remove('active');
@@ -18,7 +47,7 @@ function remAttributes() {
 
 // Change values when window is resized
 window.onresize = function () {
-  screenWidth = window.innerWidth;
+  screenWidth = window.innerWidth;;
   if (screenWidth >= 768) {
     remAttributes();
   }
@@ -45,16 +74,16 @@ addAttributes();
 
 sec.forEach((section) => {
   section.addEventListener('mouseenter', () => {
-    id = section.getAttribute('id');
-    if (id === 'about-myself') {
+    nav_id = section.getAttribute('nav_id');
+    if (nav_id === 'about-myself') {
       li[0].classList.remove('active-page');
       li[1].classList.add('active-page');
       li[2].classList.remove('active-page');
-    } else if (id === 'contact') {
+    } else if (nav_id === 'contact') {
       li[0].classList.remove('active-page');
       li[1].classList.remove('active-page');
       li[2].classList.add('active-page');
-    } else if (id != null) {
+    } else if (nav_id != null) {
       li[0].classList.add('active-page');
       li[1].classList.remove('active-page');
       li[2].classList.remove('active-page');
@@ -63,11 +92,35 @@ sec.forEach((section) => {
 });
 
 btnExpand.forEach((button) => {
-  button.addEventListener('click', () => {
-    show.classList.add('active');
+  button.addEventListener('click', (e) => {
+    const check = e.target.parentElement.parentElement.dataset.name;
+    show.classList.add('activate');
+    let title = show.getElementsByClassName('showtitle')[0];
+    let thumb = show.getElementsByClassName('showthumbnail')[0];
+    let inp = show.querySelector('.showoutside p');
+    let inpDetail = show.querySelector('.showdetails li');
+    let p = show.querySelector('.showtext p');
+    let tech = show.querySelectorAll('.showtech');
+    let btn = show.querySelectorAll('.showbuttons');
+    for(let i=0;i<4;i++){
+      if(showData['title'][i].includes(check)) {
+        title.textContent = showData['title'][i];
+        thumb.children[0].src = showData['src'][i];
+        inp.textContent = showData['detail'][0][i];
+        inpDetail.textContent = showData['detail'][1][i];
+        p.textContent = showData['info'];
+        for(let j in showData['tech']){
+          tech[0].children[j].children[0].src = showData['tech'][j];
+        }
+        for(let j in showData['buttons']){
+          btn[0].children[1].children[j].children[0].href = showData['buttons'][0][j];
+          btn[0].children[1].children[j].children[0].children[0].src = showData['buttons'][1][j];
+        }
+      }
+    }
   });
 });
 
 btnCollapse.addEventListener('click', () => {
-  show.classList.remove('active');
+  show.classList.remove('activate');
 })
